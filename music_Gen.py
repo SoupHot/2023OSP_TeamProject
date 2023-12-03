2f5eb17e16e33aca5f73b991151af3f54cfe7c53
SoupHot <hyw517232@gmail.com>
2023-12-03 22:55:06 +0900

    api 통해 이미지 텍스트에 맞는 음악 생성

--- music_Gen.py
+++ music_Gen.py
@@ -1,50 +1,39 @@
-import os
-from audiocraft.models import musicgen
-from audiocraft.data.audio import audio_write
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from transformers import AutoProcessor, MusicgenForConditionalGeneration
+import torch
+import scipy
+import time
 
-# 필요한 라이브러리를 설치하는 부분입니다. 
-# 이 부분은 비주얼 스튜디오 코드의 터미널에서 실행해야 합니다.
-# !pip install --upgrade spacy
-# !pip install --upgrade spacy==3.5.2
-# !python3 -m pip install -U git+https://github.com/facebookresearch/audiocraft#egg=audiocraft
+app = FastAPI()
+# uvicorn api:app --reload --host=0.0.0.0 --port=7342
 
-def generate_music(prompt, output_dir, output_filename_prefix, duration=30):
-    """
-    음악을 생성하고 저장하는 함수입니다.
-    
-    Args:
-    prompt (str): 음악 생성을 위한 프롬프트 텍스트.
-    output_dir (str): 생성된 음악 파일을 저장할 디렉토리 경로.
-    output_filename_prefix (str): 생성된 음악 파일의 이름 접두사.
-    duration (int, optional): 음악의 길이(초 단위). 기본값은 30초입니다.
-    """
-    # 생성할 음악 파일의 경로가 존재하지 않으면 생성합니다.
-    os.makedirs(output_dir, exist_ok=True)
+@app.get("/")
+def test():
+    return 'OSP Project API TEST(DEMO)'
 
-    # 사전 훈련된 모델을 불러옵니다.
-    # device를 'cpu'로 설정하면 CPU를 사용하여 음악을 생성합니다.
-    # device를 'cuda'로 설정하면 GPU를 사용하여 음악을 생성합니다.
-    model = musicgen.MusicGen.get_pretrained('facebook/musicgen-small', device='cpu')
+@app.get("/music-gen/{prompt}/{duration}",)
+def music_gen(prompt: str, duration: int):
+    start_time = time.time()
+    processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
+    model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
+    device = "cuda:3" if torch.cuda.is_available() else "cpu"
+    model.to(device)
 
-    # 생성 매개변수 설정
-    model.generation_params = {'use_sampling': True,
-                               'temp': 1.0,
-                               'top_k': 250,
-                               'top_p': 0.0,
-                               'cfg_coef': 3.0,
-                               'two_step_cfg': False}
-    model.set_generation_params(duration=duration)
+    inputs = processor(
+        text=[prompt],
+        padding=True,
+        return_tensors="pt",
+    )
 
-    # 음악 생성
-    output_musics = model.generate([prompt], progress=True)
+    max_new_tokens = model.config.audio_encoder.frame_rate * duration
 
-    # 생성된 음악 파일을 저장합니다.
-    for i, output_music in enumerate(output_musics):
-        output_file_path = os.path.join(output_dir, f'{output_filename_prefix}_{i}.wav')
-        audio_write(output_file_path, output_music.cpu(), model.sample_rate, strategy="loudness", loudness_compressor=True)
+    audio_values = model.generate(**inputs.to(device), max_new_tokens=max_new_tokens)
+    sampling_rate = model.config.audio_encoder.sampling_rate
+    file_path = "/home/wook/fastAPI/musicgen_out.wav"
+    scipy.io.wavfile.write(file_path, rate=sampling_rate, data=audio_values[0, 0].cpu().numpy())
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"실행 시간: {execution_time}초")
 
-# 함수 사용 예시
-file_path = r"C:\Users\8060s\OneDrive\문서\2학년 2학기\오픈소스 프로그래밍\오픈 소스 팀플\MakeTestMusic\final_music"
-prompt_text = 'I am on the battlefield and I am going to wipe out all my enemies. I am the strongest. I am full of confidence. Sparta.'
-generate_music(prompt_text, file_path, 'Sparta')
-
+    return FileResponse(file_path, media_type='audio/wav', filename="musicgen_out.wav")
